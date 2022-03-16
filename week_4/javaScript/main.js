d0edc64eea51a7502cfbc4be230b97702cb0fc84
Trailblazer007 <fatoyeoluwaseun@gmail.com>
2022-03-16 23:02:48 +0100

    Using ternary statement to execute operations

---
+++ week_4/javaScript/main.js
@@ -0,0 +1,11 @@
+//Exercises: Level 1
+// 1. Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
+
+//Using Ternary statements
+
+// adultAge = 18;
+
+// userInput = prompt("Enter your age");
+// userInput >= 18
+// ? console.log("You are old enough to drive")
+// : console.log(`You are left with ${adultAge - userInput} years to drive `);
