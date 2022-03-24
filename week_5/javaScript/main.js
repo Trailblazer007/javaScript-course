dba0a8205610daabdf3315f6a6f7335a0fd60916
Trailblazer007 <fatoyeoluwaseun@gmail.com>
2022-03-24 22:43:07 +0100

    changed all the entries in the array to upper case and printed them out individually

--- week_5/javaScript/main.js
+++ week_5/javaScript/main.js
@@ -47,7 +47,13 @@ console.log(itCompanies[6]);
 
 // 11. Change each company name to uppercase one by one and print them out
 
-
+console.log(itCompanies[0].toUpperCase());
+console.log(itCompanies[1].toUpperCase());
+console.log(itCompanies[2].toUpperCase());
+console.log(itCompanies[3].toUpperCase());
+console.log(itCompanies[4].toUpperCase());
+console.log(itCompanies[5].toUpperCase());
+console.log(itCompanies[6].toUpperCase());
 
 // 12. Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
 
