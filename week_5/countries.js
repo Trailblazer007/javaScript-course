e5ee90a76068396ec7e2d7f128825ef2eb839257
Trailblazer007 <fatoyeoluwaseun@gmail.com>
2022-04-06 23:55:43 +0100

    solved no. 2 in Ex lvl three

---
+++ week_5/countries.js
@@ -0,0 +1,14 @@
+// 1. Create a separate countries.js file and store the countries array in to this file, create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file
+const countries = [
+    'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya'
+];
--- week_5/javaScript/main.js
+++ week_5/javaScript/main.js
@@ -113,3 +113,11 @@ let itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 
 //OR
 
 // console.log(itCompanies.splice());
+
+// Ex. level 3.
+// 2. First remove all the punctuations and change the string to array and count the number of words in the array
+//  let text =
+// 'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+// const words = text.split(' ');
+// console.log(words);
+// console.log(words.length);
