a1901b23d8c468e3d859d91a59af3fe996035757
Trailblazer007 <fatoyeoluwaseun@gmail.com>
2022-04-07 18:18:00 +0100

    finished solving no. 3

--- week_5/javaScript/main.js
+++ week_5/javaScript/main.js
@@ -114,10 +114,27 @@ let itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 
 
 // console.log(itCompanies.splice());
 
-// Ex. level 3.
+// Ex. level 2.
+
 // 2. First remove all the punctuations and change the string to array and count the number of words in the array
 //  let text =
 // 'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
 // const words = text.split(' ');
 // console.log(words);
 // console.log(words.length);
+
+// 3. In the following shopping cart add, remove, edit items
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey'];
+
+// add 'Meat' in the beginning of your shopping cart if it has not been already added
+shoppingCart.unshift("Meat");
+
+// add Sugar at the end of you shopping cart if it has not been already added
+shoppingCart.push("Sugar");
+
+// remove 'Honey' if you are allergic to honey
+console.log(shoppingCart.splice(4,1));
+console.log(shoppingCart)
+
+// modify Tea to 'Green Tea'
+shoppingCart[3] = "Tea";
