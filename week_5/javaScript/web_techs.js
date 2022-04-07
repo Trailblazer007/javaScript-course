c601d87ff9df977712302b6ca6160dddefa75db3
Trailblazer007 <fatoyeoluwaseun@gmail.com>
2022-04-07 19:11:45 +0100

    solved no. 5

--- week_5/javaScript/web_techs.js
+++ week_5/javaScript/web_techs.js
@@ -7,3 +7,15 @@ const webTechs = [
   'Node',
   'MongoDB'
 ];
+
+// 5. In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
+
+// function confirm(lang) {
+//     if (webTechs.includes(lang)){
+//         return `${lang} is a CSS preprocess`
+//     }else{
+//         webTechs.push(lang);
+//     }
+// }
+// console.log(confirm('Sass'));
+// console.log(webTechs);
