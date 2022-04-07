d9349df4bc223ff47812937e264e3f5cedb501bc
Trailblazer007 <fatoyeoluwaseun@gmail.com>
2022-04-07 19:40:19 +0100

    Ex lvl 3. No. 1. Sorted the array

--- week_5/javaScript/web_techs.js
+++ week_5/javaScript/web_techs.js
@@ -21,8 +21,24 @@ const webTechs = [
 // console.log(webTechs);
 
 // 6. Concatenate the following two variables and store it in a fullStack variable.
-const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux'];
-const backEnd = ['Node','Express', 'MongoDB'];
-const fullStack = [...frontEnd, ...backEnd];
+// const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux'];
+// const backEnd = ['Node','Express', 'MongoDB'];
+// const fullStack = [...frontEnd, ...backEnd];
 
-console.log(fullStack);
+// console.log(fullStack);
+
+// Exercise Level 3;
+// 1. The following is an array of 10 students ages:
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
+
+//Sort the array and find the min and max age
+
+let sortedAges = ages.sort();
+console.log(sortedAges);
+
+
+
+// Find the median age(one middle item or two middle items divided by two)
+// Find the average age(all items divided by number of items)
+// Find the range of the ages(max minus min)
+// Compare the value of (min - average) and (max - average), use abs() method 1.Slice the first ten countries from the countries array
