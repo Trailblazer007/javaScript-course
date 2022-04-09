67883593fb6626db41fd7c0011868289a00d349d
Trailblazer007 <fatoyeoluwaseun@gmail.com>
2022-04-09 21:50:35 +0100

    solved No. 1. v

--- week_5/javaScript/web_techs.js
+++ week_5/javaScript/web_techs.js
@@ -60,20 +60,28 @@ const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
 
 
 // iii. Find the average age (all items divided by number of items)
-// let sum = 0;
-// let average = 0;
-// function arrayAverage(){
-//     for(let i = 0; i < ages.length; i++){
-//          average = (sum += ages[i]) / ages.length;
-//     } return average;
-// }
-// console.log(arrayAverage(ages));
+let sum = 0;
+let average = 0;
+function arrayAverage(){
+    for(let i = 0; i < ages.length; i++){
+         average = (sum += ages[i]) / ages.length;
+    } return average;
+}
+console.log(arrayAverage(ages));
 
 
 // iv. Find the range of the ages(max minus min)
-// let sortedAges = ages.sort();
-// let range =  sortedAges[sortedAges.length - 1] - sortedAges[0];
+let sortedAges = ages.sort();
+let range =  sortedAges[sortedAges.length - 1] - sortedAges[0];
 
-// console.log(range);
+console.log(range);
 
-// Compare the value of (min - average) and (max - average), use abs() method 1.Slice the first ten countries from the countries array
+// v Compare the value of (min - average) and (max - average), use abs() method 1.Slice the first ten countries from the countries array
+let min = sortedAges[0];
+let max = sortedAges[sortedAges.length - 1];
+
+let minAverage = Math.abs(min - average);
+let maxAverage = Math.abs(max - average);
+
+console.log(minAverage)
+console.log(maxAverage)
