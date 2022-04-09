f65b1493a6e7249884bf1d95e8ba86ef891f3e97
Trailblazer007 <fatoyeoluwaseun@gmail.com>
2022-04-10 00:13:15 +0100

    solved no 1. using the while loop

---
+++ week_6/javaScript/main.js
@@ -0,0 +1,30 @@
+//1. Iterate 0 to 10 using for loop, do the same using while and do while loop
+
+//using for loop
+const countries = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya'
+] 
+
+// Using for loop
+for(let i = 0; i<countries.length; i++){
+    console.log(countries[i])
+}
+
+// Using while loop
+
+let i = 0;
+while(i < countries.length){
+    console.log(countries[i])
+    i++
+} 
+
