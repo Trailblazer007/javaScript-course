e5c2429a677edf7919bf3ec821ec6f276e7e95c0
Trailblazer007 <fatoyeoluwaseun@gmail.com>
2022-04-11 21:51:59 +0100

    no.8. made apperance changes to the code

--- week_6/javaScript/main.js
+++ week_6/javaScript/main.js
@@ -187,8 +187,8 @@ const webTechs = [
 // }
 
 // 8. Use for loop to iterate from 0 to 100 and print only odd numbers
-for(let i=0; i<=100; i++){
-  if(i%2!==0){
+for(let i = 0; i <= 100; i++){
+  if(i % 2 !== 0){
     console.log(i)
   }
 }
