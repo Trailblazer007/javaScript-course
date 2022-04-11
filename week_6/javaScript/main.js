12d36d9e771c3896bb89f496ea0b14e679c707aa
Trailblazer007 <fatoyeoluwaseun@gmail.com>
2022-04-11 21:51:11 +0100

    no.7. made corrections to the appearance of the code

--- week_6/javaScript/main.js
+++ week_6/javaScript/main.js
@@ -179,8 +179,8 @@ const webTechs = [
 
 
 // 7. Use for loop to iterate from 0 to 100 and print only even numbers
-// for(let i=0; i<=100; i++){
-//   if(i%2==0){
+// for(let i = 0; i <= 100; i++){
+//   if(i % 2 == 0){
 //     console.log(i)
 //   }
   
