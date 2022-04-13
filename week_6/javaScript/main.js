2783a377fc458cdcce979f31630aff8b358766b9
Trailblazer007 <fatoyeoluwaseun@gmail.com>
2022-04-13 21:12:40 +0100

    solved no. 11

--- week_6/javaScript/main.js
+++ week_6/javaScript/main.js
@@ -194,21 +194,21 @@ const webTechs = [
 // }
 
 // 9. Use for loop to iterate from 0 to 100 and print only prime numbers
-let count = 0;
-let i,j 
+// let count = 0;
+// let i,j 
 
-for(j=2;j<=100;j++)
-{
-    for( i=1;i<=j;i++)
-    {
-        if(j%i==0) count++
-    }
+// for(j=2;j<=100;j++)
+// {
+//     for( i=1;i<=j;i++)
+//     {
+//         if(j%i==0) count++
+//     }
 
-    if(count==2)
-    console.log(j)
-    count=0
+//     if(count==2)
+//     console.log(j)
+//     count=0
     
-}
+// }
 
 // 10. Use for loop to iterate from 0 to 100 and print the sum of all numbers.
 // let sum = 0;
@@ -218,7 +218,23 @@ for(j=2;j<=100;j++)
 // console.log(sum)
 
 // 11. Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.
+let odd = 0;
+let even = 0;
 
+for(i=0; i<=100; i++){
+  if(i % 2 == 0){
+    even += i
+  }
+}
+for(j=0; j<=100; j++){
+  if(j % 2 !== 0){
+    odd += i
+  }
+}
+    
+
+console.log(`The sum of odd numbers are: ${odd} `);
+console.log(`The sum of even numbers are: ${even} `);
 
 
 
