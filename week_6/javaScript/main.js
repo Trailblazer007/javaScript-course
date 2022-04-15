7d314ef32800b5b6eb69edae8208c0c4c4b070f5
Trailblazer007 <fatoyeoluwaseun@gmail.com>
2022-04-16 00:35:28 +0100

    solved no. 6 in Ex. lvl 2

--- week_6/javaScript/main.js
+++ week_6/javaScript/main.js
@@ -317,6 +317,30 @@ const webTechs = [
 //   countriesLength.push(countries[k].length)
 // }console.log(countriesLength);
 
+// 6. Use the countries array to create the following array of arrays:
+
+  /*
+  [
+    ['Albania', 'ALB', 7],
+    ['Bolivia', 'BOL', 7],
+    ['Canada', 'CAN', 6],
+    ['Denmark', 'DEN', 7],
+    ['Ethiopia', 'ETH', 8],
+    ['Finland', 'FIN', 7],
+    ['Germany', 'GER', 7],
+    ['Hungary', 'HUN', 7],
+    ['Ireland', 'IRE', 7],
+    ['Iceland', 'ICE', 7],
+    ['Japan', 'JAP', 5],
+    ['Kenya', 'KEN', 5]
+  ]
+  */
+
+  const detailedArray = [];
+  for(b=0; b<countries.length; b++){
+    detailedArray.push([countries[b], countries[b].toUpperCase().slice(0,3), countries[b].length])
+  }console.log(detailedArray.slice());
+
 
 
 
