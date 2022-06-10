c22030c315eb0ae228070e96f67e70e3f5bec9b8
Trailblazer007 <fatoyeoluwaseun@gmail.com>
2022-06-10 16:40:28 +0100

    solved no. 12 of ex lvl 3

--- week_7/javaScript/main.js
+++ week_7/javaScript/main.js
@@ -661,7 +661,18 @@ function return the id. */
     // console.log(sum(4,6,7));
 
 
-// 12. Write a function called sumOfArrayItems, it takes an array parameter and return the sum of all the items. Check if all the array items are number types. If not give return reasonable feedback.
+/* 12. Write a function called sumOfArrayItems, it takes an array parameter and return the sum of all the items.
+ Check if all the array items are number types. If not give return reasonable feedback. */
+
+//     function sumOfArrayItems(arr){
+//         let sum = 0;
+//         for(element of arr){
+//             sum += element
+//         }
+//         return sum;
+//     }
+// console.log(sumOfArrayItems([2,5,3]));
+
 
 /* 13. Write a function called average, it takes an array parameter and returns the average of the items. Check if all 
 the array items are number types. If not give return reasonable feedback. */
