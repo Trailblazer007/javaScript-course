6ea9bbea68523d6a4af16939dd80ae9dd9ab49ab
Trailblazer007 <fatoyeoluwaseun@gmail.com>
2022-06-17 20:55:19 +0100

    solved no. 2

--- week_8/javaScript/main.js
+++ week_8/javaScript/main.js
@@ -1,8 +1,9 @@
 // Exercises: Level 1
 // 1. Create an empty object called dog
-//     dog = {};
+//     let dog = {};
 
 // 2. Print the the dog object on the console
+        // console.log(dog);
 
 /* 3 Add name, legs, color, age and bark properties for the dog object. The bark property is a method which return 
 woof woof */
